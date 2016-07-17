88469f49c6a51ff100026ceec5a8eb4a0d5a53fb
Caleb Morris <caleb.morris.g@gmail.com>
2016-07-17 15:05:26 -0700

    Fixed build and centrallized source files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import RSSFeed, { RSSCloud, RSSImage } from './RSSFeed';
+import RSSItem, { RSSCategory, RSSEnclosure } from './RSSItem';
+
+export {
+  RSSItem,
+  RSSEnclosure,
+  RSSCategory,
+  RSSFeed,
+  RSSCloud,
+  RSSImage,
+};
