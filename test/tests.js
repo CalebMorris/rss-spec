6f70a48814ddc67348a6ea5b8233353233ad6902
Caleb Morris <caleb.morris.g@gmail.com>
2016-07-17 13:57:58 -0700

    Added basic canLoad tests

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+var rssSpec = require('../dist');
+
+describe('Load', function() {
+  it('should contain all expected types', function() {
+    expect(rssSpec).to.be.an('object');
+    ['RSSItem', 'RSSEnclosure', 'RSSCategory', 'RSSFeed', 'RSSCloud', 'RSSImage']
+      .forEach(function(prop) {
+        expect(rssSpec).to.have.a.property(prop).that.is.a('function');
+      });
+  });
+});
